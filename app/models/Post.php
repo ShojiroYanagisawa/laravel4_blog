04f2c4d2e6aee805eb5961a4e6b532db31d0ed24
shoji <s.yanagisawa@hachimenroppi.com>
2015-01-26 15:34:02 +0900

    Add validation to Post model

--- app/models/Post.php
+++ app/models/Post.php
@@ -2,5 +2,26 @@
 
 class Post extends Eloquent
 {
+  protected $fillable = ['title', 'body'];
+  protected $errors;
 
+  public function validate(array $params)
+  {
+    $validator = Validator::make($params, [
+      'title' => 'required',
+      'body'  => 'required',
+    ]);
+
+    if ($validator->passes()) {
+      return true;
+    } else {
+      $this->errors = $validator->messages();
+      return false;
+    }
+  }
+
+  public function errors()
+  {
+    return $this->errors;
+  }
 }
