2446c31e45dbb435d3273f438e46fdf407d561fd
shoji <s.yanagisawa@hachimenroppi.com>
2015-01-26 15:31:36 +0900

    Add constructor to PostsController

--- app/controllers/PostsController.php
+++ app/controllers/PostsController.php
@@ -2,15 +2,22 @@
 
 class PostsController extends BaseController
 {
+  protected $post;
+
+  public function __construct(Post $post)
+  {
+    $this->post = $post;
+  }
+
   public function index()
   {
-    $posts = Post::all();
+    $posts = $this->post->all();
     return View::make('posts.index', ['posts' => $posts]);
   }
 
   public function show($id)
   {
-    $post = Post::findOrFail($id);
+    $post = $this->post->findOrFail($id);
     return View::make('posts.show', ['post' => $post]);
   }
 }
